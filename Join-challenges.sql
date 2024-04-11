aaab34663817ce31f9089c0ba96e3237bee05e1d
jakedomermuth <146481362+jakedomermuth@users.noreply.github.com>
2024-04-11 11:35:18 -0400

    Add files via upload

---
+++ Join-challenges.sql
@@ -0,0 +1,24 @@
+--Challenge 1: Write a query to return the following attributes for employees who belong to a department:
+--- employee_id
+--- first_name
+--- last_name
+--- salary
+--- department_name
+
+SELECT e.employee_id, e.first_name, e.last_name, e.salary, d.department_name
+FROM hcm.employees e INNER JOIN hcm.departments d
+ON e.department_id = d.department_id
+
+
+--Challenge 2:Write a query to return the following attributes for all employees, including employees who do not belong to a department:
+--- employee_id
+--- first_name
+--- last_name
+--- salary
+--- department_nameSELECT e.employee_id, e.first_name, e.last_name, e.salary, d.department_nameFROM hcm.employees e LEFT OUTER JOIN hcm.departments dON e.department_id = d.department_id--Challenge 3: Write a query to return the total number of employees in each department. Include the department_name in the query result. Also, include employees who have not been
+--assigned to a department
+
+SELECT COUNT(e.employee_id) as emp_count, d.department_name
+FROM hcm.employees e LEFT OUTER JOIN hcm.departments d
+ON e.department_id = d.department_id
+GROUP BY d.department_name
